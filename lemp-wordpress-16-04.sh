3bc795cd9c39135afbad49de11fdcfcb8c303b3a
Uğur Mirza Zeyrek <mirza@artbees.net>
2016-06-15 17:10:48 +0300

    filename changes.

--- lemp-wordpress-16-04.sh
+++ lemp-wordpress-16-04.sh
@@ -1,108 +1,235 @@
 #!/bin/sh
 
-#-- This file is a clone from https://github.com/digitalocean/do_user_scripts/blob/master/Ubuntu-14.04/cms/wordpress-nginx.sh
-# As you can expected this bash script is not working on Ubuntu 16.04 and also doesn't have PHP 7.0. Another problem is it doesn't
-# have functions unlike AbhishekGlosh's solution. It's somewhat hard to extend and customize.
-# --#
-
+# STILL ON PROGRESS
 #
 # WordPress Setup Script
 #
 # This script will install and configure WordPress on
-# an Ubuntu 14.04 droplet
+# an Ubuntu 16.04 droplet
 # Generate root and wordpress mysql passwords
-rootmysqlpass=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`;
-wpmysqlpass=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`;
 
-# Write passwords to file
-echo "MySQL Passwords for this droplet " > /etc/motd.tail;
-echo "-----------------------------------" >> /etc/motd.tail;
-echo "Root MySQL Password: $rootmysqlpass" >> /etc/motd.tail;
-echo "Wordpress MySQL Database: mwdb" >> /etc/motd.tail;
-echo "Wordpress MySQL Username: mwsql" >> /etc/motd.tail;
-echo "Wordpress MySQL Password: $wpmysqlpass" >> /etc/motd.tail;
-echo "-----------------------------------" >> /etc/motd.tail;
-echo "You can remove this information with 'rm -f /etc/motd.tail'" >> /etc/motd.tail;
-# Install mysql-server
-export DEBIAN_FRONTEND=noninteractive
-# Update Ubuntu
-apt-get update;
-apt-get -y upgrade;
-# Install Nginx/MySQL
-apt-get -y install debconf-utils
-echo mysql-server mysql-server/root_password password  | sudo debconf-set-selections
-echo mysql-server mysql-server/root_password_again password  | sudo debconf-set-selections
-sudo apt-get -y install mysql-server
-sudo apt-get install -y php5-fpm php5-mysql mysql-client unzip;
-echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
-sudo apt-get update
-sudo apt-get -y install nginx
-#start nginx
-sudo service nginx start
-# Download and uncompress WordPress
-wget https://wordpress.org/latest.zip -O /tmp/wordpress.zip;
-cd /tmp/ || exit;
-unzip /tmp/wordpress.zip;
-# Set up database user
-/usr/bin/mysqladmin -u root -h localhost create wordpress;
-/usr/bin/mysqladmin -u root -h localhost password $rootmysqlpass;
-/usr/bin/mysql -uroot -p$rootmysqlpass -e "CREATE USER wordpress@localhost IDENTIFIED BY '"$wpmysqlpass"'";
-/usr/bin/mysql -uroot -p$rootmysqlpass -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@localhost";
-# Configure PHP
-sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
-sed -i "s|listen = 127.0.0.1:9000|listen = /var/run/php5-fpm.sock|" /etc/php5/fpm/pool.d/www.conf;
-sudo service php5-fpm restart
-# Configure Nginx
-mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
-cat > /etc/nginx/sites-available/default << "EOF"
-server {
-	listen 80 default_server;
-	listen [::]:80 default_server ipv6only=on;
-	root /var/www/html;
-	index index.php index.html index.htm;
-	server_name localhost;
-	location / {
-			# First attempt to serve request as file, then
-			# as directory, then fall back to displaying a 404.
-			try_files $uri $uri/ /index.php?q=$uri&$args;
-			# Uncomment to enable naxsi on this location
-			# include /etc/nginx/naxsi.rules
-	}
-	error_page 404 /404.html;
-	error_page 500 502 503 504 /50x.html;
-	location = /50x.html {
-			root /usr/share/nginx/html;
-	}
-	location ~ \.php$ {
-			try_files $uri =404;
-			fastcgi_split_path_info ^(.+\.php)(/.+)$;
-			fastcgi_pass unix:/var/run/php5-fpm.sock;
-			fastcgi_index index.php;
-			include fastcgi.conf;
-	}
+export DEBIAN_FRONTEND=noninteractive;
+
+#initial values
+init() {
+pass_file='/root/mysql_passwd.txt'
+# leave sub_folder empty if you don't want to make installation to a sub_folder
+sub_folder=""
+web_address="localhost"
+#sub_folder="jupiter5"
+#web_address="demo2.artbees.net"
 }
-EOF
 
-cat /etc/nginx/sites-available/default
-# Add PHP info
-echo "<?php phpinfo();?>" > /var/www/html/info.php
-# Configure Nginx sites-available
-sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/wordpress
-sudo rm /etc/nginx/sites-available/default
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/wordpress
-#Configure WordPress
-cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php;
-sed -i "s|'DB_NAME', 'database_name_here'|'DB_NAME', 'wordpress'|g" /tmp/wordpress/wp-config.php;
-sed -i "s/'DB_USER', 'username_here'/'DB_USER', 'wordpress'/g" /tmp/wordpress/wp-config.php;
-sed -i "s/'DB_PASSWORD', 'password_here'/'DB_PASSWORD', '$wpmysqlpass'/g" /tmp/wordpress/wp-config.php;
-for i in `seq 1 8`
-do
-wp_salt=$(</dev/urandom tr -dc 'a-zA-Z0-9!@#$%^&*()\-_ []{}<>~`+=,.;:/?|' | head -c 64 | sed -e 's/[\/&]/\\&/g');
-sed -i "0,/put your unique phrase here/s/put your unique phrase here/$wp_salt/" /tmp/wordpress/wp-config.php;
-done
-cp -Rf /tmp/wordpress/* /var/www/html/.;
-rm -f /var/www/index.html;
-chown -Rf www-data:www-data /var/www/html;
-service nginx restart;
+#creating installation folders
+create_folders() {
+    mkdir /var/www
+    if [ "$sub_folder" != "" ]; then
+        full_path="/var/www/$web_address/$sub_folder"
+        mkdir /var/www/$web_address
+        mkdir $full_path
+    else
+        full_path="/var/www/$web_address"
+        mkdir $full_path
+    fi
+    #echo $full_path
+}
+
+#creating random passwords
+create_passwords() {
+    pass_file='/root/mysql_passwd.txt'
+    touch pass_file
+    root_mysql_passwd=`dd if=/dev/urandom bs=1 count=16 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`;
+    wp_mysql_passwd=`dd if=/dev/urandom bs=1 count=8 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`;
+    wp_mysql_user=`dd if=/dev/urandom bs=1 count=6 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`;
+    wp_database=wp_`dd if=/dev/urandom bs=1 count=6 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`;
+    echo "root mysql password: $root_mysql_passwd" >> $pass_file
+    echo "wordpress password: $wp_mysql_passwd" >> $pass_file
+    echo "wordpress user: $wp_mysql_user" >> $pass_file
+    echo "wordpress database: $wp_database" >> $pass_file
+}
+
+install_packages() {
+	#uff8_fix
+	install_php_7
+	install_mysql_57
+	install_nginx
+	install_unzip
+	restart_packages
+	echo "Installation Done."
+}
+
+restart_packages() {
+	echo "Restarting Nginx, PHP-FPM and MySQL."
+	sleep 1
+	sudo systemctl restart php7.0-fpm
+	service nginx restart
+	service mysql restart
+	echo "Packages restarted."
+}
+
+uff8_fix() {
+	# use this function if you have any utf-8 related error with ondrej/php ppa
+	echo "Fixing utf-8 error for ondrej/php package."
+	sleep 1
+	apt-get install -y language-pack-en-base
+	export LC_ALL=en_US.UTF-8
+	export LANG=en_US.UTF-8
+}
+
+general_update() {
+	echo "Updating packages."
+	sleep 1
+	apt-get update
+	apt-get -y upgrade
+}
+
+install_unzip() {
+	echo "Install unzip"
+	sleep 1
+	apt-get -y install unzip
+}
+
+install_mysql_57() {
+	echo "Installing MySQL 5.7 Server and Client"
+	sleep 1
+	apt-get -y install debconf-utils
+	echo mysql-server mysql-server/root_password password $root_mysql_passwd | sudo debconf-set-selections
+	echo mysql-server mysql-server/root_password_again password $root_mysql_passwd | sudo debconf-set-selections
+	sudo apt-get -y install mysql-server mysql-client
+	apt-get -y install mysql-server-5.7
+}
+
+install_nginx() {
+	echo "Installing Nginx webserver"
+	sleep 1
+	echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+	sudo apt-get update
+	sudo apt-get -y install nginx
+}
+
+install_php_7() {
+	echo "Adding ppa:ondrej/php repository as default PHP repository."
+	sleep 1
+	# -y flag means automatically say yes for command prompt.
+	sudo add-apt-repository -y ppa:ondrej/php
+	sudo apt-get update
+	echo "Installing PHP 7.0 packages"
+	sleep 1
+	apt-get -y install screen build-essential libcurl3 libmcrypt4 libmemcached11 libxmlrpc-epi0 php7.0-cli php7.0-common php7.0-curl php7.0-fpm php7.0-gd php7.0-intl php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-opcache php7.0-readline php7.0-xml php7.0-xmlrpc psmisc libmcrypt-dev mcrypt php-pear php-mysql php-mbstring php-mcrypt php-xml php-intl libmhash2 php-common
+}
+
+set_packages()
+{
+	#updating packages again
+	general_update
+	get_wordpress
+	set_mysql
+	set_php_7
+	set_nginx
+	set_wordpress
+}
+
+get_wordpress() {
+	# Download and uncompress WordPress
+	echo "Downloading & Unzipping WordPress Latest Release"
+	sleep 1
+	wget https://wordpress.org/latest.zip -O /tmp/wordpress.zip;
+	cd /tmp/ || exit;
+	unzip /tmp/wordpress.zip;
+}
+
+set_mysql() {
+	echo "Set up database user"
+	sleep 1
+	# Set up database user
+	root_mysql_passwd=`sed -n "s/^.*root mysql password:\s*\(\S*\).*$/\1/p" $pass_file`;
+	wp_mysql_passwd=`sed -n "s/^.*wordpress password:\s*\(\S*\).*$/\1/p" $pass_file`;
+	wp_mysql_user=`sed -n "s/^.*wordpress user:\s*\(\S*\).*$/\1/p" $pass_file`;
+	wp_database=`sed -n "s/^.*wordpress database:\s*\(\S*\).*$/\1/p" $pass_file`;
+	echo $root_mysql_passwd
+	echo $wp_mysql_passwd
+	echo $wp_mysql_user
+	echo $wp_database
+	/usr/bin/mysqladmin -u root -h localhost create $wp_database -p$root_mysql_passwd;
+	/usr/bin/mysql -uroot -p$root_mysql_passwd -e "CREATE USER $wp_mysql_user@localhost IDENTIFIED BY '"$wp_mysql_passwd"'";
+	/usr/bin/mysql -uroot -p$root_mysql_passwd -e "GRANT ALL PRIVILEGES ON $wp_database.* TO $wp_mysql_user@localhost";
+}
+
+set_php_7() {
+	echo "set php 7"
+	sleep 1
+	# Configure PHP by mostly increasing default variables!
+	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 20M/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/post_max_size = 8M/post_max_size = 20M/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/max_input_time = 60/max_input_time = 120/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/; max_input_vars = 1000/max_input_vars = 4000/" /etc/php/7.0/fpm/php.ini
+	#sed -i "s|listen = 127.0.0.1:9000|listen = /var/run/php5-fpm.sock|" /etc/php5/fpm/pool.d/www.conf;
+	sudo systemctl restart php7.0-fpm
+}
+
+set_nginx() {
+	echo "Configuring nginx settings"
+	sleep 1
+	cp -avr /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+	cp -avr /etc/nginx/sites-available/default.bak /etc/nginx/sites-available/$web_address
+    #rm -avr /etc/nginx/sites-available/default
+    # making default settings
+	sed -i "s/try_files \$uri \$uri\/ =404;/try_files \$uri \$uri\/ \/index.php\$is_args\$args;/" /etc/nginx/sites-available/$web_address
+	sed -i "s/server_name _;/server_name $web_address;/" /etc/nginx/sites-available/$web_address
+	sed -i "s/root \/var\/www\/html;/root \/var\/www\/$web_address;/" /etc/nginx/sites-available/$web_address
+	sed -i "s/index index.html/index index.php index.html/" /etc/nginx/sites-available/$web_address
+	# replacing default server if it's not localhost
+	if [ "$web_address" != "localhost" ]; then
+	sed -i "s/listen 80 default_server;/listen 80;/" /etc/nginx/sites-available/$web_address
+	sed -i "s/listen \[\:\:\]\:80 default_server;/listen \[\:\:\]\:80;/" /etc/nginx/sites-available/$web_address
+	fi
+    # adding .php connection to the nginx
+    sed -i "s/#location ~ \\\.php\$ {/location ~ \\\.php\$ {/" /etc/nginx/sites-available/$web_address
+    sed -i "s/#\tinclude snippets\/fastcgi-php.conf;/\tinclude snippets\/fastcgi-php.conf;/" /etc/nginx/sites-available/$web_address
+    sed -i "s/#\tfastcgi_pass unix:\/var\/run\/php7.0-fpm.sock;/\tfastcgi_pass unix:\/run\/php\/php7.0-fpm.sock;\n\t\tinclude fastcgi_params;/" /etc/nginx/sites-available/$web_address
+    # blocking access from .htaccess
+    sed -i "s/#location ~ \/\\\.ht {/location ~ \/\\\.ht {/" /etc/nginx/sites-available/$web_address
+    sed -i "s/#\tdeny all;/\tdeny all;/" /etc/nginx/sites-available/$web_address
+    # closing the gap for previous nginx and htaccess settings
+    sed -i "s/\t#}/\t}/" /etc/nginx/sites-available/$web_address
+	mv /etc/nginx/sites-available/$web_address /etc/nginx/sites-enabled/$web_address
+	touch /var/www/$web_address/info.php
+	echo "<?php phpinfo();?>" > /var/www/$web_address/info.php
+	echo /var/www/$web_address/info.php
+	rm /etc/nginx/sites-enabled/default
+	sudo systemctl reload nginx
+}
+
+set_wordpress() {
+	echo "Configuring WordPress"
+	sleep 1
+    mv /tmp/wordpress/* $full_path
+    cp $full_path/wp-config-sample.php $full_path/wp-config.php;
+    sed -i "s|'DB_NAME', 'database_name_here'|'DB_NAME', '$wp_database'|g" $full_path/wp-config.php;
+    sed -i "s/'DB_USER', 'username_here'/'DB_USER', '$wp_mysql_user'/g" $full_path/wp-config.php;
+    sed -i "s/'DB_PASSWORD', 'password_here'/'DB_PASSWORD', '$wp_mysql_passwd'/g" $full_path/wp-config.php;
+    for i in `seq 1 8`
+    do
+    wp_salt=$(</dev/urandom tr -dc 'a-zA-Z0-9!@#$%^&*()\-_ []{}<>~`+=,.;:/?|' | head -c 64 | sed -e 's/[\/&]/\\&/g');
+    sed -i "0,/put your unique phrase here/s/put your unique phrase here/$wp_salt/" $full_path/wp-config.php;
+    done
+    chown -Rf www-data:www-data $full_path;
+}
+
+rm_temp() {
+	echo "Removing temp files"
+	sleep 1
+    rm -rf /tmp/wordpress
+    rm /tmp/wordpress.zip
+}
+
+#uff8_fix
+init
+create_folders
+create_passwords
+install_packages
+set_packages
+rm_temp
